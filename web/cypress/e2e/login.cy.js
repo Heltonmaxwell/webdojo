868c00ff8c457ef3a4abe7a8e9d914e9224c5273
Helton Maxwell <Heltonmaxwell.info@gmail.com>
2025-08-06 18:52:10 -0300

    Realização do primeiro teste

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', ()=>{
+  it('Realizando login de conta.',()=>{
+    cy.viewport(1440,900)
+    cy.visit('http://localhost:3000')
+
+    //primeiro teste
+
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('katana123')
+    cy.contains('button','Entrar').click()
+    
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible') // O comando should indica a espera de um resultado, nesse caso, que o componente esteja visível.
+      .and('have.text','Fernando Papito') // ja nesse caso, espera que o componente possua o valor seguinte
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text','Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
   })
 })
